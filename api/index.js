a75724e5d6ec345cdaac9b5e6f4acab9c5b49169
Puruu Puruu <nueapi11@gmail.com>
2025-06-28 20:37:10 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,7 @@
 const app = require('express')();
-const { v4 } = require('uuid');
 
 app.get('/', (req, res) => {
-  const path = `/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`<p>Hello! Go to item: <a href="${path}">${path}</a></p>`);
-});
-
-app.get('/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`<p>Item: ${slug}</p><a href="/">Go back</a>`);
-});
-
-app.get('/api/redirect', async (req,res) => {
-  const code = req.query.code;
-  console.log('code:', code);
-  const next = req.query.next ?? 'https://vercel.com';
-  console.log('redirect url:', next);
-  res.redirect(next);
-})
+  res.end("Gomen Amanai")
+  });
 
 module.exports = app;
